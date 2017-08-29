f48ccbd2af6c0e95a99af26c26c874b4ce785979
antifreezzz <vlaklementev@yandex.ru>
2017-08-29 14:59:13 +0300

    Initial commit

---
+++ src/Dialogs/Dialog1.java
@@ -0,0 +1,123 @@
+package Dialogs;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Dialog1 extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton addButton;
+    private JButton clearButton;
+    private int Field3Flag = 0;
+
+
+    public Dialog1() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+        setSize(300, 300);
+
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onAdd();
+            }
+        });
+
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onClear();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onClear() {
+        textField1.setText("");
+        textField2.setText("");
+        textField3.setText("");
+    }
+
+    public void onAdd() {
+        float num1, num2, result;
+        num1 = Float.parseFloat(textField1.getText());
+        num2 = Float.parseFloat(textField2.getText());
+        result = num1 + num2;
+        textField3.setText(String.valueOf(result));
+        Field3Flag = 1;
+
+    }
+
+    public void onOK() {
+
+        try  {
+{
+
+            FileWriter writer = new FileWriter("src/Data/results.txt", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+            bufferedWriter.write(String.valueOf(Float.parseFloat(textField3.getText())) + "\n");
+
+            bufferedWriter.close();
+            dispose();}
+
+        } catch (IOException ex) {
+
+            dispose();
+        }
+
+    }
+
+    private void onCancel() {
+
+        if (textField3.getText() != null) {
+            CancelWithResult cancelWithResult = new CancelWithResult();
+            cancelWithResult.setVisible(true);
+        } else dispose();
+
+
+    }
+
+    public static void main(String[] args) {
+        Dialog1 dialog = new Dialog1();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
---
+++ src/Sandbox1.java
@@ -0,0 +1,12 @@
+import Dialogs.Dialog1;
+
+/**
+ * Created by Klementyev on 29.08.2017.
+ */
+public class Sandbox1 {
+    public static void main(String[] args){
+        Dialog1 dialog1 = new Dialog1();
+        dialog1.setVisible(true);
+
+    }
+}
