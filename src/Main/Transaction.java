0378d9ca01c82e10a687d3ab8fb3727e8e4a83f9
antifreezzz <vlaklementev@yandex.ru>
2017-09-17 22:40:48 +0300

    SQLITE

---
+++ src/Main/Transaction.java
@@ -0,0 +1,29 @@
+package Main;
+
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
+import java.util.Date;
+
+public class Transaction {
+    public int ID;
+    public String Name;
+    public String Type;
+    public String Cathegory;
+    public double Sum;
+    public Date date;
+
+    public Transaction(int ID, String Name, String Type, String Cathegory, double Sum, Date date){
+        this.ID = ID;
+        this.Name = Name;
+        this.Type = Type;
+        this.Cathegory = Cathegory;
+        this.Sum = Sum;
+        this.date = date;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("ID: %s | Имя: %s | Тип: %s | Категория: %s | Сумма: %s | Дата: %s ",
+                this.ID, this.Name, this.Type, this.Cathegory, this.Sum, this.date);
+    }
+}
